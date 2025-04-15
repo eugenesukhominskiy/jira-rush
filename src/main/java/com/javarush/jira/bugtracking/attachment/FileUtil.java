8488a6b8971c9bf4cdc2b20b2769fcf21e810ff6
Eugene <yevheniisukhominskyi@gmail.com>
2025-04-15 18:15:24 +0300

    3. Refactored the com.javarush.jira.bugtracking.attachment.FileUtil#upload method

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -25,14 +25,17 @@ public class FileUtil {
             throw new IllegalRequestDataException("Select a file to upload.");
         }
 
-        File dir = new File(directoryPath);
-        if (dir.exists() || dir.mkdirs()) {
-            File file = new File(directoryPath + fileName);
-            try (OutputStream outStream = new FileOutputStream(file)) {
-                outStream.write(multipartFile.getBytes());
-            } catch (IOException ex) {
-                throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
+        Path dirPath = Paths.get(directoryPath);
+
+        try {
+            if (Files.notExists(dirPath)) {
+                Files.createDirectories(dirPath);
             }
+
+            Path filePath = dirPath.resolve(fileName);
+            Files.write(filePath, multipartFile.getBytes());
+        } catch (IOException ex) {
+            throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
         }
     }
 
